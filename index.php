6c8632a86cad21be46e5e94a157bd47b72c97b23
LUIS CARLOS DA SILVA DIAS <luisdias04@users.noreply.github.com>
2021-01-03 15:39:07 -0300

    index

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 <pre>
 <?php
 
-//require __dir__."/vendor/autoload.php"; 
+//require __dir__ . "/vendor/autoload.php"; 
 //->não esta funcionando preciso instalar ainda
 
 require_once("autoload.php");
